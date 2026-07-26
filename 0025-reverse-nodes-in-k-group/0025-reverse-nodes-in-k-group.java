09b63bc4e9672bfb29358afd4f10cee6aae164b6
suprithack03 <supritha03ck@gmail.com>
2026-07-27 00:07:08 +0530

    Time: 0 ms (100%), Space: 46.6 MB (17.18%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    // Reverse a linked list
+    public ListNode reverse(ListNode head) {
+
+        ListNode prev = null;
+        ListNode temp = head;
+
+        while (temp != null) {
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        return prev;
+    }
+
+    // Find the kth node from head
+    public ListNode findKthNode(ListNode head, int k) {
+
+        while (head != null && --k > 0) {
+            head = head.next;
+        }
+
+        return head;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode temp = head;
+        ListNode prevLast = null;
+
+        while (temp != null) {
+
+            // Find kth node
+            ListNode kthNode = findKthNode(temp, k);
+
+            // Less than k nodes left
+            if (kthNode == null) {
+
+                if (prevLast != null)
+                    prevLast.next = temp;
+
+                break;
+            }
+
+            // Save next group's starting node
+            ListNode nextNode = kthNode.next;
+
+            // Break the current group
+            kthNode.next = null;
+
+            // First group
+            if (temp == head) {
+                head = reverse(temp);
+            }
+            else {
+                prevLast.next = reverse(temp);
+            }
+
+            // temp becomes the last node after reversing
+            prevLast = temp;
+
+            // Connect with next group
+            temp.next = nextNode;
+
+            // Move to next group
+            temp = nextNode;
+        }
+
+        return head;
+    }
+}
