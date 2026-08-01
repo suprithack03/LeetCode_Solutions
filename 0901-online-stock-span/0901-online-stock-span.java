6448daa15355358e669e297f69224a487fa4e133
suprithack03 <supritha03ck@gmail.com>
2026-08-01 23:04:54 +0530

    Time: 36 ms (13.7%), Space: 54.4 MB (60.75%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,24 @@
+class StockSpanner {
+
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+
+            span += st.peek()[1];
+
+            st.pop();
+        }
+
+        st.push(new int[]{price, span});
+
+        return span;
+    }
+}
